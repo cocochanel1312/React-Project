24b956b55f560b3db79902aea0f42e1a9521ade9
Konstantin <k.kalnizki13@gmail.com>
2024-04-24 17:04:48 +0600

    feat: Editing assortment using props

--- src/App.js
+++ src/App.js
@@ -18,15 +18,14 @@ function App() {
           </div>
           <h2 class="content__title">Все пиццы</h2>
           <div class="content__items">
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
+            <PizzaBlock title="Морской узел" price="4.99"/>
+            <PizzaBlock title="4 мать его сыра" price="2.99"/>
+            <PizzaBlock title="Хэллоупенья" price="3.99"/>
+            <PizzaBlock title="Острый Мексиканец" price="5.99"/>
+            <PizzaBlock title="Омичка" price="9.99"/>
+            <PizzaBlock title="Крабово-креветочная" price="13.99"/>
+            <PizzaBlock title="Торпедуха" price="8.99"/>
+            <PizzaBlock title="Мясная" price="7.99"/>
           </div>
         </div>
       </div>
