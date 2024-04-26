6a576daf266ff1f62631e176f4560ed16f985494
Konstantin <k.kalnizki13@gmail.com>
2024-04-26 20:35:48 +0600

    feat: Reducing the code in the Categories component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 
-
 function Categories() {
-  const [activeIndexCategory, setActiveIndexCategory] = useState(0)
+  const [activeIndexCategory, setActiveIndexCategory] = useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
 
   const onClickCategory = (index) => {
-    setActiveIndexCategory(index)
-  }
+    setActiveIndexCategory(index);
+  };
 
   return (
     <div className="categories">
       <ul>
-        <li onClick={() => onClickCategory(0)}  className={activeIndexCategory === 0 ? "active" : ""}>Все</li>
-        <li onClick={() => onClickCategory(1)} className={activeIndexCategory === 1 ? "active" : ""}>Мясные</li>
-        <li onClick={() => onClickCategory(2)} className={activeIndexCategory === 2 ? "active" : ""}>Вегетарианская</li>
-        <li onClick={() => onClickCategory(3)} className={activeIndexCategory === 3 ? "active" : ""}>Гриль</li>
-        <li onClick={() => onClickCategory(4)} className={activeIndexCategory === 4 ? "active" : ""}>Острые</li>
-        <li onClick={() => onClickCategory(5)} className={activeIndexCategory === 5 ? "active" : ""}>Закрытые</li>
+        {
+          categories.map((value, index) => (
+            <li onClick={() => onClickCategory(index)} className={activeIndexCategory === index ? 'active' : ''}>
+              {value}
+            </li>
+          ))}
       </ul>
     </div>
   );
