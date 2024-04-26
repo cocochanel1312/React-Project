becee8b60fa8382e5b337d590952fddcac0b9bc0
Konstantin <k.kalnizki13@gmail.com>
2024-04-26 19:29:56 +0600

    feat: Adding logic to the pizza category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,22 @@
+import React, { useState } from "react";
+
+
 function Categories() {
+  const [activeIndexCategory, setActiveIndexCategory] = useState(0)
+
+  const onClickCategory = (index) => {
+    setActiveIndexCategory(index)
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li onClick={() => onClickCategory(0)}  className={activeIndexCategory === 0 ? "active" : ""}>Все</li>
+        <li onClick={() => onClickCategory(1)} className={activeIndexCategory === 1 ? "active" : ""}>Мясные</li>
+        <li onClick={() => onClickCategory(2)} className={activeIndexCategory === 2 ? "active" : ""}>Вегетарианская</li>
+        <li onClick={() => onClickCategory(3)} className={activeIndexCategory === 3 ? "active" : ""}>Гриль</li>
+        <li onClick={() => onClickCategory(4)} className={activeIndexCategory === 4 ? "active" : ""}>Острые</li>
+        <li onClick={() => onClickCategory(5)} className={activeIndexCategory === 5 ? "active" : ""}>Закрытые</li>
       </ul>
     </div>
   );
