b9b37672accb4e014f3b614b28de3618b3179547
Konstantin <k.kalnizki13@gmail.com>
2024-04-27 18:04:21 +0600

    feat: Changing PizzaBlock using json file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import pizzas from "./assets/pizza.json";
+
+console.log(pizzas);
 
 function App() {
   return (
@@ -17,15 +20,13 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock title="Морской узел" price="4.99" />
-            <PizzaBlock title="4 мать его сыра" price="2.99" />
-            <PizzaBlock title="Хэллоупенья" price="3.99" />
-            <PizzaBlock title="Острый Мексиканец" price="5.99" />
-            <PizzaBlock title="Омичка" price="9.99" />
-            <PizzaBlock title="Крабово-креветочная" price="13.99" />
-            <PizzaBlock title="Торпедуха" price="9.99" />
-            <PizzaBlock title="Мясная" price="7.99" />
-            <PizzaBlock title="Whaaaaat?" price="0.99" />
+            {pizzas.map((object) => (
+              <PizzaBlock
+                title={object.title}
+                price={object.price}
+                image={object.imageUrl}
+              />
+            ))}
           </div>
         </div>
       </div>
