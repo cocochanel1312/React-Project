c30ea0f43dd175e4e3fae92115fe2ef533d98d5d
Konstantin <k.kalnizki13@gmail.com>
2024-05-03 19:55:52 +0600

    feat: created scss module and using react-routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -11,7 +12,10 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <NotFound />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
