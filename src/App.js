d861fc55c7024afc94dcb28771b05d90f6d75fe4
Konstantin <k.kalnizki13@gmail.com>
2024-05-06 18:49:04 +0600

    feat: creation of adaptive layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./scss/app.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
